a46d97240df027974c37c21ab409a0529131da02
Adam Fortuna <adam@fortuna.name>
2017-12-20 14:13:27 -0700

    working

--- app/fizzbuzz.js
+++ app/fizzbuzz.js
@@ -1,9 +1,6 @@
 module.exports = function(print) {
   for(var i=1; i<=100; i++) {
-    if(i==93) {
-      print("nope");
-    }
-    else if((i%3 === 0) && (i%5 === 0)) {
+    if((i%3 === 0) && (i%5 === 0)) {
       print("FizzBuzz");
     } else if(i%5 === 0) {
       print("Buzz");
